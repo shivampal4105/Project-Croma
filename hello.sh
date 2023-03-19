ac0bc3f25d0d3a4e8da33e4c6612a2c038bd34b0
Shaik Abdul Vahid <59871200+SAW-NAAZ@users.noreply.github.com>
2023-03-19 13:07:23 +0530

    Modified on Github
    Hi Abdul This is Vahid there was a error in the code, i have been update 
    Kindly check

--- hello.sh
+++ hello.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo "Hello and Good afternoon"
 echo "And Welcome to the session"
-
-
+echo "Hello and Good afternoon"
+echo "And Welcome to the session"echo "Hello and Good afternoon"
+echo "And Welcome to the session"echo "Hello and Good afternoon"
+echo "And Welcome to the session"echo "Hello and Good afternoon"
+echo "And Welcome to the session"
+echo "Hello and Good afternoon"
+echo "And Welcome to the session"
